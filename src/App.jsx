b7761aecb4f5e5a2518afb5b863d325b000755d9
galicians <galicians@gmail.com>
2025-11-26 11:29:11 +0000

    UI reading data from JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Entry from './components/Entry'
+import articles from './data.js';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const entryElements = articles.map((article) => {
+    return React.createElement(Entry,article)
+  })
+
 
   return (
     <>
-   <Header />
-     <Entry 
-      img={{
-        src: "https://scrimba.com/links/travel-journal-japan-image-url",
-        alt: "Mount Fuji"
-      }}
-     title='Mount Fuji'
-     country="Japan"
-     googleMapsLink='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu'
-     dates="12 Jan, 2021 - 24 Jan, 202"
-     text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-     
-     />
+    <Header />
+   {entryElements.map((newEntry) => { return newEntry})}
+  
     </>
   )
 }
